57122052f63bbd054e0ca84d3c6832e6ecb00ec8
prnake <prnake@gmail.com>
2021-07-24 17:01:18 +0800

    fix: encoding problem
    not really fixed, just skip the wrong part

--- mysite/scrapy/views.py
+++ mysite/scrapy/views.py
@@ -82,173 +82,177 @@ class ThuSpider(object):
                 'p_sort.asc2': 'true', 'p_xnxq': 2021-2022-1, 'pathContent': '%D2%BB%BC%B6%BF%CE%BF%AA%BF%CE%D0%C5%CF%A2', 'goPageNumber': '1'}
         for count in range(start, end):
             print("正在处理第%d页" % count)
-            data['page'] = count
-            response = requests.post(self.url, data=data, headers=self.headers)
-            if response.status_code == 200:
-                soup = BeautifulSoup(response.text, 'html.parser')
-                a = soup.find_all("tr", class_="trr2")
-                for item in a:
-                    b = [""] * 20
-                    detail = dict()
-                    row = item.select('td')
-                    for i, word in enumerate(row):
-                        b[i] = re.sub(r"\s", "", word.get_text())
-                    try:
-                        detail['class_url'] = self.base_url + \
-                            row[3].find('a')['href']
-                    except:
-                        detail['class_url'] = ''
-                    detail['department'] = b[0]
-                    detail['class_id'] = b[1]
-                    detail['class_alter_id'] = b[2]
-                    detail['title'] = b[3]
-                    detail['study_credit'] = b[4]
-                    detail['teacher'] = b[5]
-                    try:
-                        detail['teacher_url'] = self.base_url + \
-                            row[5].find('a')['href']
-                    except:
-                        detail['teacher_url'] = ''
-                    detail['a12'] = b[6]
-                    detail['a13'] = b[7]
-                    detail['a14'] = b[8]
-                    detail['a15'] = b[9]
-                    detail['a16'] = b[10]
-                    detail['a17'] = b[11]
-                    detail['a18'] = b[12]
-                    detail['a19'] = b[13]
-                    detail['a20'] = b[14]
-                    detail['a21'] = b[15]
-                    detail['a22'] = b[16]
-                    if detail['class_url']:
-                        class_r = requests.get(
-                            detail['class_url'], headers=self.headers)
-                        if response.status_code == 200:
-                            html = etree.HTML(class_r.text)
-                            html = etree.tostring(html)
-                            class_soup = BeautifulSoup(html, 'html.parser')
-                            c = class_soup.select(
-                                'table.table-striped > tr > td')
-                            d = [""] * 40
-                            if len(c) > 37:
-                                print(detail['class_url'], "因格式问题跳过")
-                                detail['body'] = ''
-                                detail['study_hour'] = ''
-                                detail['introduction'] = ''
-                                detail['en_introduction'] = ''
-                                detail['a1'] = ''
-                                detail['a2'] = ''
-                                detail['a3'] = ''
-                                detail['a4'] = ''
-                                detail['a5'] = ''
-                                detail['a6'] = ''
-                                detail['a7'] = ''
-                                detail['a8'] = ''
-                                detail['a9'] = ''
-                                detail['a10'] = ''
-                                detail['a11'] = ''
-                            else:
-                                for i, item in enumerate(c):
-                                    if i in [10, 12, 14]:
-                                        d[i] = item.get_text().strip() if re.sub(
-                                            r"\s", "", item.get_text()) else ""
-                                    else:
-                                        d[i] = re.sub(
-                                            r"\s", "", item.get_text())
-                                detail['body'] = d[0]
-                                detail['study_hour'] = d[6]
-                                detail['introduction'] = d[10]
-                                detail['en_introduction'] = d[12]
-                                detail['a1'] = d[14]
-                                detail['a2'] = d[16]
-                                detail['a3'] = d[18]
-                                detail['a4'] = d[20]
-                                detail['a5'] = d[22]
-                                detail['a6'] = d[24]
-                                detail['a7'] = d[26]
-                                detail['a8'] = d[28]
-                                detail['a9'] = d[30]
-                                detail['a10'] = d[32]
-                                detail['a11'] = d[34]
-                        else:
-                            print("Cookie已失效")
-                            return None
-                    else:
-                        detail['body'] = ''
-                        detail['study_hour'] = ''
-                        detail['introduction'] = ''
-                        detail['en_introduction'] = ''
-                        detail['a1'] = ''
-                        detail['a2'] = ''
-                        detail['a3'] = ''
-                        detail['a4'] = ''
-                        detail['a5'] = ''
-                        detail['a6'] = ''
-                        detail['a7'] = ''
-                        detail['a8'] = ''
-                        detail['a9'] = ''
-                        detail['a10'] = ''
-                        detail['a11'] = ''
-                    if detail['teacher_url']:
-                        teacher_r = requests.get(
-                            detail['teacher_url'], headers=self.headers)
-                        if response.status_code == 200:
-                            class_soup = BeautifulSoup(
-                                teacher_r.text, 'html.parser')
-                            c = class_soup.select(
-                                'table.table-striped > tbody > tr > td')
-                            if len(c) > 22:
-                                print(detail['teacher_url'], "因格式问题跳过")
-                                detail['teacher_id'] = ''
-                                detail['teacher_name'] = ''
-                                detail['teacher_sex'] = ''
-                                detail['teacher_a1'] = ''
-                                detail['teacher_a2'] = ''
-                                detail['teacher_a3'] = ''
-                                detail['teacher_a4'] = ''
-                                detail['teacher_a5'] = ''
-                                detail['teacher_a6'] = ''
-                                detail['teacher_a7'] = ''
-                            else:
+            try:
+                data['page'] = count
+                response = requests.post(self.url, data=data, headers=self.headers)
+                if response.status_code == 200:
+                    soup = BeautifulSoup(response.text, 'html.parser')
+                    a = soup.find_all("tr", class_="trr2")
+                    for item in a:
+                        b = [""] * 20
+                        detail = dict()
+                        row = item.select('td')
+                        for i, word in enumerate(row):
+                            b[i] = re.sub(r"\s", "", word.get_text())
+                        try:
+                            detail['class_url'] = self.base_url + \
+                                row[3].find('a')['href']
+                        except:
+                            detail['class_url'] = ''
+                        detail['department'] = b[0]
+                        detail['class_id'] = b[1]
+                        detail['class_alter_id'] = b[2]
+                        detail['title'] = b[3]
+                        detail['study_credit'] = b[4]
+                        detail['teacher'] = b[5]
+                        try:
+                            detail['teacher_url'] = self.base_url + \
+                                row[5].find('a')['href']
+                        except:
+                            detail['teacher_url'] = ''
+                        detail['a12'] = b[6]
+                        detail['a13'] = b[7]
+                        detail['a14'] = b[8]
+                        detail['a15'] = b[9]
+                        detail['a16'] = b[10]
+                        detail['a17'] = b[11]
+                        detail['a18'] = b[12]
+                        detail['a19'] = b[13]
+                        detail['a20'] = b[14]
+                        detail['a21'] = b[15]
+                        detail['a22'] = b[16]
+                        if detail['class_url']:
+                            class_r = requests.get(
+                                detail['class_url'], headers=self.headers)
+                            if response.status_code == 200:
+                                html = etree.HTML(class_r.text)
+                                html = etree.tostring(html)
+                                class_soup = BeautifulSoup(html, 'html.parser')
+                                c = class_soup.select(
+                                    'table.table-striped > tr > td')
                                 d = [""] * 40
-                                for i, item in enumerate(c):
-                                    if i in [5, 17]:
-                                        d[i] = item.get_text().strip() if re.sub(
-                                            r"\s", "", item.get_text()) else ""
-                                    else:
-                                        d[i] = re.sub(
-                                            r"\s", "", item.get_text())
-                                detail['teacher_id'] = d[1]
-                                detail['teacher_name'] = d[5]
-                                detail['teacher_sex'] = d[7]
-                                detail['teacher_a1'] = d[9]
-                                detail['teacher_a2'] = d[11]
-                                detail['teacher_a3'] = d[13]
-                                detail['teacher_a4'] = d[15]
-                                detail['teacher_a5'] = d[17]
-                                detail['teacher_a6'] = d[19]
-                                detail['teacher_a7'] = d[21]
+                                if len(c) > 37:
+                                    print(detail['class_url'], "因格式问题跳过")
+                                    detail['body'] = ''
+                                    detail['study_hour'] = ''
+                                    detail['introduction'] = ''
+                                    detail['en_introduction'] = ''
+                                    detail['a1'] = ''
+                                    detail['a2'] = ''
+                                    detail['a3'] = ''
+                                    detail['a4'] = ''
+                                    detail['a5'] = ''
+                                    detail['a6'] = ''
+                                    detail['a7'] = ''
+                                    detail['a8'] = ''
+                                    detail['a9'] = ''
+                                    detail['a10'] = ''
+                                    detail['a11'] = ''
+                                else:
+                                    for i, item in enumerate(c):
+                                        if i in [10, 12, 14]:
+                                            d[i] = item.get_text().strip() if re.sub(
+                                                r"\s", "", item.get_text()) else ""
+                                        else:
+                                            d[i] = re.sub(
+                                                r"\s", "", item.get_text())
+                                    detail['body'] = d[0]
+                                    detail['study_hour'] = d[6]
+                                    detail['introduction'] = d[10]
+                                    detail['en_introduction'] = d[12]
+                                    detail['a1'] = d[14]
+                                    detail['a2'] = d[16]
+                                    detail['a3'] = d[18]
+                                    detail['a4'] = d[20]
+                                    detail['a5'] = d[22]
+                                    detail['a6'] = d[24]
+                                    detail['a7'] = d[26]
+                                    detail['a8'] = d[28]
+                                    detail['a9'] = d[30]
+                                    detail['a10'] = d[32]
+                                    detail['a11'] = d[34]
+                            else:
+                                print("Cookie已失效")
+                                return None
                         else:
-                            print("Cookie已失效")
-                            return None
-                    else:
-                        detail['teacher_id'] = ''
-                        detail['teacher_name'] = ''
-                        detail['teacher_sex'] = ''
-                        detail['teacher_a1'] = ''
-                        detail['teacher_a2'] = ''
-                        detail['teacher_a3'] = ''
-                        detail['teacher_a4'] = ''
-                        detail['teacher_a5'] = ''
-                        detail['teacher_a6'] = ''
-                        detail['teacher_a7'] = ''
-                    self.data.append(detail)
-                    # print(detail)
+                            detail['body'] = ''
+                            detail['study_hour'] = ''
+                            detail['introduction'] = ''
+                            detail['en_introduction'] = ''
+                            detail['a1'] = ''
+                            detail['a2'] = ''
+                            detail['a3'] = ''
+                            detail['a4'] = ''
+                            detail['a5'] = ''
+                            detail['a6'] = ''
+                            detail['a7'] = ''
+                            detail['a8'] = ''
+                            detail['a9'] = ''
+                            detail['a10'] = ''
+                            detail['a11'] = ''
+                        if detail['teacher_url']:
+                            teacher_r = requests.get(
+                                detail['teacher_url'], headers=self.headers)
+                            if response.status_code == 200:
+                                class_soup = BeautifulSoup(
+                                    teacher_r.text, 'html.parser')
+                                c = class_soup.select(
+                                    'table.table-striped > tbody > tr > td')
+                                if len(c) > 22:
+                                    print(detail['teacher_url'], "因格式问题跳过")
+                                    detail['teacher_id'] = ''
+                                    detail['teacher_name'] = ''
+                                    detail['teacher_sex'] = ''
+                                    detail['teacher_a1'] = ''
+                                    detail['teacher_a2'] = ''
+                                    detail['teacher_a3'] = ''
+                                    detail['teacher_a4'] = ''
+                                    detail['teacher_a5'] = ''
+                                    detail['teacher_a6'] = ''
+                                    detail['teacher_a7'] = ''
+                                else:
+                                    d = [""] * 40
+                                    for i, item in enumerate(c):
+                                        if i in [5, 17]:
+                                            d[i] = item.get_text().strip() if re.sub(
+                                                r"\s", "", item.get_text()) else ""
+                                        else:
+                                            d[i] = re.sub(
+                                                r"\s", "", item.get_text())
+                                    detail['teacher_id'] = d[1]
+                                    detail['teacher_name'] = d[5]
+                                    detail['teacher_sex'] = d[7]
+                                    detail['teacher_a1'] = d[9]
+                                    detail['teacher_a2'] = d[11]
+                                    detail['teacher_a3'] = d[13]
+                                    detail['teacher_a4'] = d[15]
+                                    detail['teacher_a5'] = d[17]
+                                    detail['teacher_a6'] = d[19]
+                                    detail['teacher_a7'] = d[21]
+                            else:
+                                print("Cookie已失效")
+                                return None
+                        else:
+                            detail['teacher_id'] = ''
+                            detail['teacher_name'] = ''
+                            detail['teacher_sex'] = ''
+                            detail['teacher_a1'] = ''
+                            detail['teacher_a2'] = ''
+                            detail['teacher_a3'] = ''
+                            detail['teacher_a4'] = ''
+                            detail['teacher_a5'] = ''
+                            detail['teacher_a6'] = ''
+                            detail['teacher_a7'] = ''
+                        self.data.append(detail)
+                        # print(detail)
+                else:
+                    print("Cookie已失效")
+                    return None
+            except:
+                print("第%d页处理出现错误" % count)
             else:
-                print("Cookie已失效")
-                return None
-            print("第%d页处理完成" % count)
-            self.save_data_to_model()
+                print("第%d页处理完成" % count)
+                self.save_data_to_model()
 
     def save_data_to_model(self):
         try:
